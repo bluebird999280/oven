a65d53a8d7073975b3078f037697b7b1450415e1
bluebird999280 <bluebird999280@gmail.com>
2025-05-29 17:39:59 +0900

    @Refactor loginApi를 AuthApi로 합침침

---
+++ apis/auth.ts
@@ -0,0 +1,54 @@
+import axios from "@apis/instance";
+
+interface ILoginApiData {
+    accessToken : string,
+    refreshToken : string,
+    nickname : string,
+    userId : string
+}
+
+export async function loginApi(username : string, password : string) : Promise<ILoginApiData> {
+    try {
+        const response = await axios.post(
+            "/auth/login",
+            { data : { username, password } }
+        )
+
+        return response.data;
+    } catch(e : any) {
+        if(!e.errorMessage) {
+            throw e.errorMessage;
+        }
+        throw "알 수 없는 오류가 발생했습니다."
+    }
+}
+
+export async function checkUsernameApi(username : string) : Promise<boolean> {
+    try {
+        const response = await axios.get("/auth/checkUsername", {
+            data : { username }
+        })
+
+        return response.status === 200;
+    } catch(e : any) {
+        if(!e.errorMessage) {
+            throw e.errorMessage;
+        }
+        throw "알 수 없는 오류가 발생했습니다."
+    }
+}
+
+export async function registerApi(name : string, username : string, password : string, workIdList : any) {
+    try {
+        const response = await axios.post("/auth/register", {
+            data : { name, username, password, workIdList }
+        })
+
+        return response.data;
+    } catch(e : any) {
+        if(!e.errorMessage) {
+            throw e.errorMessage;
+        }
+        throw "알 수 없는 오류가 발생했습니다."
+    }
+}
