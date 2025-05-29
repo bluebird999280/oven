ccbbe32413a485f3ea168b9a50fc96ff4fbd03d3
bluebird999280 <bluebird999280@gmail.com>
2025-05-30 04:13:10 +0900

    @Design DearkRed 색상 추가가

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,5 +1,6 @@
 export const ORANGE = '#ff8232';
 export const RED = '#F6342B';
+export const DARKRED = "#AE0C18";
 export const BROWN = '#4f2416';
 export const BEIGE = '#f4efe7';
 export const GREEN = '#1A7721';
