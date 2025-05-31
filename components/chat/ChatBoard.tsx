82ff199702257d07b92dd6f25b9310cf25f05896
bluebird999280 <bluebird999280@gmail.com>
2025-06-01 03:33:21 +0900

    @Design ChatBoard 디자인 완성

---
+++ components/chat/ChatBoard.tsx
@@ -0,0 +1,60 @@
+import React, { useRef, useState } from 'react';
+import {
+    Dimensions,
+    NativeScrollEvent,
+    NativeSyntheticEvent,
+    StyleSheet,
+    View
+} from 'react-native';
+import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated';
+import Chat from './Chat';
+
+interface IProps {
+    chatList: string[]
+}
+
+export default function ChatBoard({ chatList }: IProps) {
+    const chatScrollRef = useRef<ScrollView | null>(null);
+    const chatContentRef = useRef<View | null>(null);
+    const [totalChatHeight, setTotalChatHeight] = useState(0);
+
+    const onScrollChat = (e : NativeSyntheticEvent<NativeScrollEvent>) => {
+        if (e?.nativeEvent.contentOffset.y == 0) {
+            setTotalChatHeight(e.nativeEvent.contentSize.height);
+        }
+    };
+
+    const onChangeChatSize = (contentWidth : number, contentHeight : number ) => {
+        chatContentRef.current?.measure((x, y, width, height) => {
+            chatScrollRef.current?.scrollTo({
+                x: 0,
+                y: height - totalChatHeight,
+                animated: false,
+            });
+        });
+    };
+
+    return (
+        <View>
+            <ScrollView 
+                ref={chatScrollRef}
+                style={styles.wrapper}
+                onScroll={onScrollChat}
+                onContentSizeChange={onChangeChatSize}
+                scrollEventThrottle={36}
+                automaticallyAdjustKeyboardInsets={true}
+            >
+                <View ref={chatContentRef}>
+                    <Chat chatList={chatList} />
+                </View>
+            </ScrollView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    wrapper: {
+        width: "100%",
+        height: Dimensions.get("window").height - 300
+    }
+})
